680aee784c6b835e3da3fdd26a093d4494c46441
Iñaki Yabar <inayabarb@gmail.com>
2018-10-08 11:05:38 -0300

    Added blank matrix verif

--- app/controllers/mutants_controller.rb
+++ app/controllers/mutants_controller.rb
@@ -1,5 +1,6 @@
 class MutantsController < ApplicationController
   def check_dna
+    return render json: { error: 'A matrix is required' }, status: :bad_request if params[:dna].blank?
     return render json: { result: 'Mutant verified' }, status: :ok if DnaChecker.new.is_mutant?(params[:dna])
     render json: { result: 'Not a mutant' }, status: :forbidden
   rescue DnaCheckerErrors::Standard => e
