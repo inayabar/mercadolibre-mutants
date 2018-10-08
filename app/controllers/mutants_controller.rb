d35a2a153a75a714c0bda8b4bc05c3d57123da87
Iñaki Yabar <inayabarb@gmail.com>
2018-10-08 11:03:07 -0300

    Added result messages in mutants controller

--- app/controllers/mutants_controller.rb
+++ app/controllers/mutants_controller.rb
@@ -1,7 +1,7 @@
 class MutantsController < ApplicationController
   def check_dna
-    return render json: {}, status: :ok if DnaChecker.new.is_mutant?(params[:dna])
-    render json: {}, status: :forbidden
+    return render json: { result: 'Mutant verified' }, status: :ok if DnaChecker.new.is_mutant?(params[:dna])
+    render json: { result: 'Not a mutant' }, status: :forbidden
   rescue DnaCheckerErrors::Standard => e
     render json: { error: e.message }, status: :bad_request
   end
